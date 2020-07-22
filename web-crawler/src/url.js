7312752c51cf84cd241b5fcd07c679fe83414a6b
chrika <g.tchrikishvili.exceedteam@gmail.com>
2020-07-22 18:55:31 +0400

    changed apiurl

--- web-crawler/src/url.js
+++ web-crawler/src/url.js
@@ -1,2 +1,2 @@
-// export const apiUrl = 'https://web-crawler-api-3.herokuapp.com/crawler-api';
-export const apiUrl = 'http://localhost:5000/crawler-api';
+export const apiUrl = 'https://web-crawler-api-3.herokuapp.com/crawler-api';
+// export const apiUrl = 'http://localhost:5000/crawler-api';
